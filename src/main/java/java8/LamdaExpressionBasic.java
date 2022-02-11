d33040deee3c9b509fbdd4345def4a4df207cdfa
sharath1493 <sharath1493@gmail.com>
2022-02-11 22:07:54 +0530

    added java class where basic lambda exp. used implementing an Interface

---
+++ src/main/java/java8/LamdaExpressionBasic.java
@@ -0,0 +1,20 @@
+package java8;
+
+public class LamdaExpressionBasic {
+
+	public static void main(String[] args) {
+
+
+		/*
+		 * Points while using lambda expression
+		 * 1. Interface has to functional interface i.e. only one abstract method in interface
+		 * 2. Parameters and return type should match
+		 * 3. Symbol "->" will be used for lambda expression 
+		 */
+
+		IPrintable i = () -> System.out.println("from the lambda expression");
+
+		i.print();
+	}
+
+}
