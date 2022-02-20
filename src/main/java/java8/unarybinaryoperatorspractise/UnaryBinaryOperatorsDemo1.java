1c0f2c82e7a8e70a671ac2071de1a9ea3fffbce6
sharath1493 <sharath1493@gmail.com>
2022-02-20 19:26:42 +0530

    updated unary/binary practise demo class

--- src/main/java/java8/unarybinaryoperatorspractise/UnaryBinaryOperatorsDemo1.java
+++ src/main/java/java8/unarybinaryoperatorspractise/UnaryBinaryOperatorsDemo1.java
@@ -9,6 +9,25 @@ public class UnaryBinaryOperatorsDemo1 {
 
 		List<Integer> l = Arrays.asList(24, 5, 7, 3, 678, 56, 4, 46);
 
+		List<String> l1= Arrays.asList("Anna","Nelly","NBull","Billy");
+
+
+		//------------ Unary operator --------------------- 
+
+		//normal way
+
+		l1.stream().forEach(s -> System.out.println(s.toUpperCase()));
+
+		//Converting a list of String to upper case with unary operator with ReplaceAll method
+		l1.replaceAll(s->s.toUpperCase());
+
+		System.out.println(l1);
+
+
+
+
+		//------------ Binary operator --------------------- 
+
 		// Normal way of streaming
 		System.out.println(l.stream().mapToInt(s -> s.intValue()).sum());
 
